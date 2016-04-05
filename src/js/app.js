3301e556862e57255e1a27b7b503cca6995121b9
maxbarry <maxbarrymail@gmail.com>
2016-04-05 23:19:09 +0100

    Made it in to more of a demo

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
-// Custom modules
-require("./_modernizr.custom");
-require("./_unlock")();
-require("./_emphasis_lines")();
-require("./_waypoints.settings")();
-require("./_image__ui")();
+var $ = require("jquery");
+
+var init = function() {
+    var vidId = Math.floor((Math.random() * 5) + 1);
+    $("video").html('<source src="/video/' + vidId + '.mp4" type="video/mp4"></source>' );
+
+    // $("body").addClass("--unlock");
+};
+
+init();
