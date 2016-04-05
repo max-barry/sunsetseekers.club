db50940ccd52ad8aaa9fd0a62870ecdaef996952
maxbarry <maxbarrymail@gmail.com>
2016-04-05 23:39:28 +0100

    Updated readme. Mobile for the text size

--- Readme.md
+++ Readme.md
@@ -1,6 +1,17 @@
 # SunsetSeekers.club
 One day this will be a site about sunsets. Until then, it's just goofing around with blend modes and video.
 
+[sunseekers.club](http://sunseekers.club)
+
+All the text is live. There are pseudo elements with the bands over the text which combines with the blend mode to make something unorthodox.
+
+```
+h1 {
+    font-size: 17vw;
+    mix-blend-mode: overlay;
+}
+```
+
 ## Branches
 `kitchen-sink` contains the entire original project including:
  - "Fullscreen to unlock" experience
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,24 @@
 var $ = require("jquery");
 
+// Randomise video
 var init = function() {
     var vidId = Math.floor((Math.random() * 5) + 1);
     $("video").html('<source src="/video/' + vidId + '.mp4" type="video/mp4"></source>' );
-
-    // $("body").addClass("--unlock");
 };
 
 init();
+
+// Mousemove credit show
+var to,
+    $a = $("a");
+
+var showCredits = function() {
+    clearTimeout(to);
+    $a.show();
+    
+    to = setTimeout(function(){
+        $a.hide();
+    }, 2000);
+};
+
+$("body").mousemove(showCredits);
